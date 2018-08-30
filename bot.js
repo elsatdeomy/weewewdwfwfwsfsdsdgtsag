fe61c7fa82726a43b804c634313cf738044ec5ff
elsatdeomy <42783125+elsatdeomy@users.noreply.github.com>
2018-08-30 04:37:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,4 +28,29 @@ client.user.setGame(`OF A7 OF A7`,"http://twitch.tv/S-F")
   console.log('')
 });
 
+client.on('message', msg => {
+	var prefix = "B.";
+  if (msg.author.bot) return;
+  if (!msg.content.startsWith(prefix)) return;
+  let command = msg.content.split(" ")[0];
+  command = command.slice(prefix.length);
+  let args = msg.content.split(" ").slice(1);
+
+    if(command === "clr") {
+        const emoji = client.emojis.find("name", "wastebasket")
+    let textxt = args.slice(0).join("");
+    if(msg.member.hasPermission("MANAGE_MESSAGES")) {
+    if (textxt == "") {
+        msg.delete().then
+    msg.channel.send("***```Supply A Number ??```***").then(m => m.delete(3000));
+} else {
+    msg.delete().then
+    msg.delete().then
+    msg.channel.bulkDelete(textxt);
+        msg.channel.send("```Cleard: " + textxt + "\n Messages```").then(m => m.delete(3000));
+        }
+    }
+}
+});
+
 client.login(process.env.BOT_TOKEN);
