f8ee65b444e11a2f2cd1ad0c0beacff1fb9ef064
elsatdeomy <42783125+elsatdeomy@users.noreply.github.com>
2018-08-30 21:13:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -233,7 +233,6 @@ ${user} انت معاقب بميوت كتابي بسبب مخالفة القوا
  
 });
 
-const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
 const moment = require('moment');
