a0be7cd9bcbefb5e337b31960debe123593b2075
elsatdeomy <42783125+elsatdeomy@users.noreply.github.com>
2018-09-01 22:10:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ const client = new Discord.Client({disableEveryone: true});
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`By:Ahmed|`,"http://twitch.tv/S-F")
+client.user.setGame(`By:Ahmed |`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
