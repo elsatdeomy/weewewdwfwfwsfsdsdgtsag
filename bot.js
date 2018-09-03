2fdd626efc51f7621b9619891cc0a57308bdf53c
elsatdeomy <42783125+elsatdeomy@users.noreply.github.com>
2018-09-04 01:01:51 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -595,18 +595,4 @@ hours = 12;
  
 });
 
- client.on('message', message => {
-   if(message.content.startsWith(prefix + "7rf")) {
- let args = message.content.split(' ').slice(1);
-message.channel.sendMessage("𝓒𝓗𝓐𝓣𝓣𝓘𝓝𝓖-شات", {embed: {
-      title: `tag`,
-      color: 0x06DF00,
-      description: `\n ${zalgo(args.join(' '))}`,
-     
-    }
-    });
-  }
-});
-
-
 client.login(process.env.BOT_TOKEN);
