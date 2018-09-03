65ef7b325c1806606186e6973d2e6f17ea6f22a6
elsatdeomy <42783125+elsatdeomy@users.noreply.github.com>
2018-09-04 01:37:00 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -601,7 +601,7 @@ hours = 12;
  message.channel.sendMessage("", {embed: {
       title: `tag`,
       color: 0x06DF00,
-      description: `\n ${zalgo(args.join(' '))}`,
+      description: `\n ${client(args.join(' '))}`,
      
     }
     });
