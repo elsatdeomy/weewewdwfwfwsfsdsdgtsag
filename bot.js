d1ac056c8daa5c7797d0d456d66f1b4e34346a77
elsatdeomy <42783125+elsatdeomy@users.noreply.github.com>
2018-09-04 02:26:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -601,7 +601,7 @@ hours = 12;
  message.channel.sendMessage("", {embed: {
       title: `tag`,
       color: 0x06DF00,
-      description: `\n ${utill(args.join(' '))}`,
+      description: `\ ${utill(args.join(' '))}`,
      
     }
     });
