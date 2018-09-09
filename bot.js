7adf1bfef9aacefe30957ce3a261cbf9210f1643
elsatdeomy <42783125+elsatdeomy@users.noreply.github.com>
2018-09-09 02:35:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1333,44 +1333,6 @@ client.on('message', message => {
     }) 
     }
     });
-client.on('message', message => {
-    if (!points[message.author.id]) points[message.author.id] = { 
-        points: 0,
-      };
-    if (message.content == "G.ايموجى") { 
-        if(!message.channel.guild) return message.reply('**هذا الأمر للسيرفرات فقط**').then(m => m.delete(3000));
-    
-    const type = require('./emojis.json'); 
-    const item = type[Math.floor(Math.random() * type.length)]; 
-    const filter = response => { 
-        return item.answers.some(answer => answer.toLowerCase() === response.content.toLowerCase());
-    };
-   message.channel.send({embed: new Discord.RichEmbed().setTitle('لديك ثانيه للاجابه بالجواب الصحيح').setThumbnail(`${item.type}`)}).then(function(m) {
-             setTimeout(function() {
-m.edit({embed: new Discord.RichEmbed().setTitle('لديك 15 ثانيه للاجابه بالجواب الصحيح').setThumbnail('https://images-ext-2.discordapp.net/external/lLOYcLfSQaNo_5Ex0I-gBD5lIW-FfRXO-W_-ZxSpYLA/https/i.imgur.com/iReHvIZ.png?width=100&height=100')})
-             }, 1000)
-            message.channel.awaitMessages(filter, { maxMatches: 1, time: 15000, errors: ['time'] })
-            .then((collected) => {
-            const sh = new Discord.RichEmbed()
-.setColor("04791c")
-.setDescription('**✅ |Good Job +1P**')
-.addField('Type G.mypoints', 'To Show ur Points' , true)
-.setFooter(message.author.username, message.author.avatarURL)
-message.channel.sendEmbed(sh);
-            console.log(`[Game] ${collected.first().author} Answered with the correct answer`);
-                let won = collected.first().author;
-                points[won.id].points++;
-              })
-              .catch(collected => { 
-                message.channel.send(`:x: **لم يقم أحد بكتابة الايموجي بالوقت المناسب**`);
-                console.log(`[Game] No one answered the correct answer`);
-                    })
-                     fs.writeFile("./points.json", JSON.stringify(points), (err) => {
-    if (err) console.error(err)
-  })
-            })
-    }
-    });
 client.on("message", function(message) {
    if(message.content.startsWith(prefix + "rps")) {
     let messageArgs = message.content.split(" ").slice(1).join(" ");
