1a6bad1da98c5c11084bb9d791fa6a20cfe19bdc
elsatdeomy <42783125+elsatdeomy@users.noreply.github.com>
2018-09-10 00:14:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3102,7 +3102,6 @@ client.on('guildMemberAdd', member => {
      const welcomer =  member.guild.channels.find('name', 'ا𝐜𝐡𝐚𝐭');
 const w = ['./w1.png'];
  
-         let Image = Canvas.Image,
             canvas = new Canvas(400, 200),
             ctx = canvas.getContext('2d');
         fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
